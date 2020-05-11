d756e918bdee8c70e6ad33f93c84eac45322b063
yuskillupup <yuworkeryu55@gmail.com>
2020-05-11 16:02:54 +0900

    ヘッダー修正

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 // トップページ
 import Head from 'next/head'
 import Link from 'next/link'
-import utilStyles from '../public/css/utils.module.css' 
+import utilStyles from '../public/css/utils.module.css'
 import React from 'react'
 
 //App.js
@@ -18,11 +18,85 @@ class Divider extends React.Component{
           <Head>
             <title>Index.js</title>
             <link rel="icon" href="/favicon.ico" />
+            <link rel="stylesheet" href="/css/header.css" />
           </Head>
+          <header>
+            <div className="header__logo">
+            <Link href="#">
+              <a>
+                <h1 >Engineer</h1>  
+              </a>
+            </Link>
+            </div>
+            <nav className="header__menu">
+              <ul className="header__menu__flexbox">
+                <li>
+                  <Link href="#">
+                    <a>
+                      <img src='/images/nav-list.png' alt="list"/>
+                      <span>一覧へ</span>
+                    </a>
+                  </Link>                
+                </li>
+                <li>
+                  <Link href="#">
+                    <a>
+                      <img src='/images/nav-question.png' alt="question"/>
+                      <span>よくある質問</span>
+                    </a>
+                  </Link>
+                                  
+                </li>
+                <li>
+                  <Link href="#">
+                    <a>
+                      <img src='/images/nav-slide.png' alt="slide"/>
+                      <span>スライド字幕</span>
+                    </a>
+                  </Link>
+                                  
+                </li>
+                <li>
+                  <Link href="#">
+                    <a>
+                      <img src='/images/nav-movie.png' alt="movie"/>
+                      <span>動画字幕</span>
+                    </a>
+                  </Link>
+                                  
+                </li>
+                <li>
+                  <Link href="#">
+                    <a>
+                      <img src='/images/nav-copy.png' alt="copy"/>
+                      <span>写経</span>
+                    </a>
+                  </Link>
+                                  
+                </li>
+                <li>
+                  <Link href="#">
+                    <a>
+                      <img src='/images/nav-practice.png' alt="practice"/>
+                      <span>練習問題</span>
+                    </a>
+                  </Link>
+                                  
+                </li>
+              </ul>
+            </nav>
+            <div className="header__login">
+              <Link href="#">
+                    <a>
+                      <p>ログイン<span>▼</span></p>
+                    </a>
+              </Link>
+            </div>
+          </header>
     
           <main>
             <h1 className="title">
-              Cognito　×　React　×　Next.js<br/><Link href="/posts/signup"><a>Let's signup!</a></Link>
+              React　×　Next.js<br/><Link href="/posts/signup"><a>Let's signup!</a></Link>
             </h1>
             {/* <form onSubmit={this.submitHandler}> */}
             <form>
